1a2ea76c8cdba43af487ad58052206b8b45f1870
Sori <robertogarzajoa@gmail.com>
2025-03-03 14:50:07 -0600

    Animate Hands + Socket

--- Assets/Scripts/AnimateHandsModels.cs
+++ Assets/Scripts/AnimateHandsModels.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+[System.Serializable]
+public class AnimateHandsModelOnInput
+{
+    public string animationProperty;
+    public InputActionProperty actionProperty;
+}
 
 public class AnimateHandsModels : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] List<AnimateHandsModelOnInput> animationInputs;
+    [SerializeField] Animator animator; 
+    
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (AnimateHandsModelOnInput animationInput in animationInputs)
+        {
+            float actionValue = animationInput.actionProperty.action.ReadValue<float>();
+            animator.SetFloat(animationInput.animationProperty, actionValue);
+        }
     }
 }
